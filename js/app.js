8765caf246b8e79aac353ef5c1686f2442e3924b
DmitryGrechko <grechkoda@gmail.com>
2017-07-14 12:41:35 -0400

    Made toggleDrawing Function using knockout

--- js/app.js
+++ js/app.js
@@ -58,17 +58,6 @@ function initMap() {
     searchBox.setBounds(map.getBounds());
 
     var largeInfowindow = new google.maps.InfoWindow();
-    // Initialize the drawing manager.
-    var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_LEFT,
-            drawingModes: [
-                google.maps.drawing.OverlayType.POLYGON
-            ]
-        }
-    });
     // Style the markers a bit. This will be our listing marker icon.
     var defaultIcon = makeMarkerIcon('0091ff');
     // Create a "highlighted location" marker color for when the user
@@ -103,41 +92,12 @@ function initMap() {
             this.setIcon(defaultIcon);
         });
     });
-    document.getElementById('toggle-drawing').addEventListener(
-        'click',
-        function() {
-            toggleDrawing(drawingManager);
-        });
     // Listen for the event fired when the user selects a prediction from the
     // picklist and retrieve more details for that place.
     searchBox.addListener('places_changed', function() {
         searchBoxPlaces(this);
     });
-    // Add an event listener so that the polygon is captured,  call the
-    // searchWithinPolygon function. This will show the markers in the polygon,
-    // and hide any outside of it.
-    drawingManager.addListener('overlaycomplete', function(event) {
-        // First, check if there is an existing polygon.
-        // If there is, get rid of it and remove the markers
-        if (polygon) {
-            polygon.setMap(null);
-            hideMarkers(markers);
-        }
-        // Switching the drawing mode to the HAND (i.e., no longer drawing).
-        drawingManager.setDrawingMode(null);
-        // Creating a new editable polygon from the overlay.
-        polygon = event.overlay;
-        polygon.setEditable(true);
-        // Searching within the polygon.
-        searchWithinPolygon(polygon);
-        // Make sure the search is re-done if the poly is changed.
-        polygon.getPath().addListener('set_at',
-            searchWithinPolygon);
-        polygon.getPath().addListener('insert_at',
-            searchWithinPolygon);
-    });
 }
-
 
 // Return a city name that matches a marker id
 function getCityName(locations, marker) {
@@ -210,18 +170,7 @@ function makeMarkerIcon(markerColor) {
         new google.maps.Size(21, 34));
     return markerImage;
 }
-// This shows and hides (respectively) the drawing options.
-function toggleDrawing(drawingManager) {
-    if (drawingManager.map) {
-        drawingManager.setMap(null);
-        // In case the user drew anything, get rid of the polygon
-        if (polygon !== null) {
-            polygon.setMap(null);
-        }
-    } else {
-        drawingManager.setMap(map);
-    }
-}
+
 // This function hides all markers outside the polygon,
 // and shows only the ones within it. This is so that the
 // user can specify an exact area of search.
@@ -587,6 +536,52 @@ function viewModel() {
     self.hideMarkers = function() {
         for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(null);
+        }
+    };
+    // This shows and hides (respectively) the drawing options.
+    self.toggleDrawing = function() {
+        // Initialize the drawing manager.
+        var drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.POLYGON,
+            drawingControl: true,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_LEFT,
+                drawingModes: [
+                    google.maps.drawing.OverlayType.POLYGON
+                ]
+            }
+        });
+        // Add an event listener so that the polygon is captured,  call the
+        // searchWithinPolygon function. This will show the markers in the polygon,
+        // and hide any outside of it.
+        drawingManager.addListener('overlaycomplete', function(event) {
+            // First, check if there is an existing polygon.
+            // If there is, get rid of it and remove the markers
+            if (polygon) {
+                polygon.setMap(null);
+                hideMarkers(markers);
+            }
+            // Switching the drawing mode to the HAND (i.e., no longer drawing).
+            drawingManager.setDrawingMode(null);
+            // Creating a new editable polygon from the overlay.
+            polygon = event.overlay;
+            polygon.setEditable(true);
+            // Searching within the polygon.
+            searchWithinPolygon(polygon);
+            // Make sure the search is re-done if the poly is changed.
+            polygon.getPath().addListener('set_at',
+                searchWithinPolygon);
+            polygon.getPath().addListener('insert_at',
+                searchWithinPolygon);
+        });
+        if (drawingManager.map) {
+            drawingManager.setMap(null);
+            // In case the user drew anything, get rid of the polygon
+            if (polygon !== null) {
+                polygon.setMap(null);
+            }
+        } else {
+            drawingManager.setMap(map);
         }
     };
 }
